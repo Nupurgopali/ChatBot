f20922ff7dc6d21076f5f27717514a709871f894
Nupur <nurrygopali@gmail.com>
2020-05-05 00:26:01 +0530

    Changed the contents of app.py file

--- Bot/app.py
+++ Bot/app.py
@@ -4,8 +4,8 @@ app = Flask(__name__);
 @app.route("/Bot", methods=["POST"])
 def response():
     query = dict(request.form)['query']
-    if(query=='Hi')
-    {res='Hello'}
+    if(query=='Hi'):
+        res='Hello'
     return jsonify({"response" : res})
 if __name__=="__main__":
     app.run(host="0.0.0.0",)
